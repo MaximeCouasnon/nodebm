ed7645dd47ef4fd5343c3d6601a9531b094ce98f
Maxime Couasnon <maxime.couasnon.2013@utt.fr>
2016-07-11 23:26:36 +0200

    Test unitaire du calcul du dû.

---
+++ source/test/calculs/index.js
@@ -0,0 +1,44 @@
+import test from 'tape';
+import { calculDuDu } from 'shared/calculs';
+
+test( "Calcul du dû pour une personne sur une dépense", assert => {
+  let args;
+
+  args = {
+    partsUtilisateur: 1,
+    prix: 100,
+    nombreDeParts: 5
+  };
+  assert.equal(
+    calculDuDu( args ), 20,
+    "Dépense de 100€ et je possède 1 part sur 5 : dû = 20€" );
+
+  args = {
+    partsUtilisateur: 0,
+    prix: 100,
+    nombreDeParts: 5
+  };
+  assert.equal(
+    calculDuDu( args ), 0,
+    "Si j'ai 0 parts : je paye 0€" );
+
+  args = {
+    partsUtilisateur: 1,
+    prix: 0,
+    nombreDeParts: 5
+  };
+  assert.equal(
+    calculDuDu( args ), 0,
+    "Prix 0€ : je paye 0€" );
+
+  args = {
+    partsUtilisateur: 0,
+    prix: 0,
+    nombreDeParts: 0
+  };
+  assert.equal(
+    calculDuDu( args ), false,
+    "On interdit qu'il y ait 0 parts en tout" );
+
+  assert.end();
+} );
