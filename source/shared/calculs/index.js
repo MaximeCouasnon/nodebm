f09cefc89ba5ad11471472fc92bab16800c64b73
MAXIME COUASNON <maxime.couasnon-prestataire@laposte.fr>
2016-07-19 19:20:41 +0200

    Beginning English translation

--- source/shared/calculs/index.js
+++ source/shared/calculs/index.js
@@ -1,68 +1,76 @@
 /**
- * Fonction pour Array.prototype.reduce : somme des valeurs d'un tableau
+ * Functional sum for Array.prototype.reduce
+ * @param {Number} a
+ * @param {Number} b
+ * @return {Number} a + b
  */
-const sum = ( a, b ) => a + b;
+const sum = ( a, b ) => ( Number( a ) || 0 ) + ( Number( b ) || 0 );
 
 /**
  * Calculer le dû pour une personne sur une dépense à plusieurs
  *
- * @param {Object} depense - Une ligne de dépense du groupe
- * @param {Array} depense.parts - Tableau des parts de chacun dans la dépense
- * @param {Number} depense.prix - Prix total de la dépense
- * @param {String} idUtilisateur - Identifiant de la personne dont on calcule le
+ * @param {Object} expense - Une ligne de dépense du groupe
+ * @param {Array} expense.shares - Tableau des parts de chacun dans la dépense
+ * @param {Number} expense.price - Prix total de la dépense
+ * @param {String} userId - Identifiant de la personne dont on calcule le
  * dû sur cette dépense
  * @return {Number} - Le dû de la personne
  */
-const calculDuDu = ( { parts = [], prix = 0 }, idUtilisateur ) => {
-  const nombreDeParts = parts.length;
-
-  // Il est anormal qu'il y ait 0 parts sur une dépense
-  // on retourne 0 au lieu de NaN
-  if ( !nombreDeParts ) {
+const calculDuDu = ( { shares, price = 0 }, userId ) => {
+  // Only accept an Array
+  //(otherwise a String could break the function with its length property)
+  if ( !Array.isArray( shares ) ) {
     return 0;
   }
 
-  const partsUtilisateur = parts
+  const totalNumberOfShares = shares.length;
+
+  // If shares is empty (which would be weird), return 0 instead of NaN
+  if ( totalNumberOfShares === 0 ) {
+    return 0;
+  }
+
+  const userShares = shares
     .map( id => {
-      if ( id === idUtilisateur ) {
+      if ( id === userId ) {
         return 1
       }
       return 0;
     } )
     .reduce( sum );
 
-  return partsUtilisateur * prix / nombreDeParts;
+  return userShares * price / totalNumberOfShares;
 };
 
 /**
  * Faire la somme des dûs d'un utilisateur sur un groupe
  *
- * @param {Array} groupe - Un tableau de dépenses
- * @param {String} idUtilisateur - Identifiant de la personne dont on calcule la
+ * @param {Array} group - Un tableau de dépenses
+ * @param {String} userId - Identifiant de la personne dont on calcule la
  * somme des dûs sur ce groupe
  */
-const sommeDesDus = ( groupe = [], idUtilisateur ) => {
-  return groupe
-    .map( depense => calculDuDu( depense, idUtilisateur ) )
+const sommeDesDus = ( group = [], userId ) => {
+  return group
+    .map( expense => calculDuDu( expense, userId ) )
     .reduce( sum );
 };
 
 /**
  * Faire la somme des dépenses d'un utilisateur sur un groupe
  *
- * @param {Array} groupe - Un tableau de dépenses
- * @param {String} idPayeur - Identifiant de la personne dont on calcule la
+ * @param {Array} group - Un tableau de dépenses
+ * @param {String} payerId - Identifiant de la personne dont on calcule la
  * somme des paiements sur ce groupe
  */
-const sommeDesDepenses = ( groupe = [], idPayeur ) => {
-  if ( !groupe.length ) {
+const sommeDesDepenses = ( group = [], payerId ) => {
+  if ( !group.length ) {
     return 0;
   }
 
-  return groupe
-    .map( depense => {
-      if ( depense.idPayeur === idPayeur ) {
-        return depense.prix;
+  return group
+    .map( expense => {
+      if ( expense.payerId === payerId ) {
+        return expense.price;
       }
       return 0;
     } )
--- source/test/calculs/index.js
+++ source/test/calculs/index.js
@@ -6,104 +6,110 @@ import {
   sommeDesDepenses
 } from 'shared/calculs';
 
-test( "Calcul du dû pour une personne sur une dépense", assert => {
-  let depense;
+test( "Calculate what a given user owes for an expense", assert => {
+  let expense;
 
-  depense = {
-    parts: [ "Bill", "Billy", "Jim", "Jimmy", "Jimmy" ],
-    prix: 100
+  expense = {
+    shares: [ "Bill", "Billy", "Jim", "Jimmy", "Jimmy" ],
+    price: 100
   };
   assert.equal(
-    calculDuDu( depense, "Bill" ), 20,
-    "Dépense de 100€ et je possède 1 part sur 5 : je dois 20€" );
+    calculDuDu( expense, "Bill" ), 20,
+    "If I got 1 share out of 5 on a 100 expense: then I owe 20" );
 
   assert.equal(
-    calculDuDu( depense, "Jimmy" ), 40,
-    "Dépense de 100€ et je possède 2 parts sur 5 : je dois 40€" );
+    calculDuDu( expense, "Jimmy" ), 40,
+    "If I got 2 shares out of 5 on a 100 expense: then I owe 40" );
 
   assert.equal(
-    calculDuDu( depense, "Thomas A. Edison" ), 0,
-    "Si j'ai 0 parts : je dois 0€" );
+    calculDuDu( expense, "Thomas A. Edison" ), 0,
+    "If I got 0 shares: then I owe 0" );
 
-  depense = {
-    parts: [ "Bill", "Jimmy", "Tommy" ],
-    prix: 0
+  expense = {
+    shares: [ "Bill", "Jimmy", "Tommy" ],
+    price: 0
   };
   assert.equal(
-    calculDuDu( depense, "Bill" ), 0,
-    "Prix 0€ : je dois 0€" );
+    calculDuDu( expense, "Bill" ), 0,
+    "If price is 0: then I owe 0" );
 
-  depense = {
-    parts: [],
-    prix: 6510
+  expense = {
+    shares: [],
+    price: 6510
   };
   assert.equal(
-    calculDuDu( depense ), 0,
-    "S'il y a 0 parts en tout (anormal), je dois 0€" );
+    calculDuDu( expense ), 0,
+    "Abnormal case: 'shares' is an empty array" );
+
+  expense = {
+    shares: null,
+    price: 6510
+  };
+  assert.equal(
+    calculDuDu( expense ), 0,
+    "Abnormal case: 'shares' is null" );
 
   assert.end();
 } );
 
 test( "Calcul de la somme des dûs d'une personne sur un groupe", assert => {
-  let groupe;
+  let group;
 
-  groupe = [ {
-    parts: [ "Bill", "Billy", "Jim", "Jimmy", "Jimmy" ],
-    prix: 100
+  group = [ {
+    shares: [ "Bill", "Billy", "Jim", "Jimmy", "Jimmy" ],
+    price: 100
   } ];
-  assert.equal( sommeDesDus( groupe, "Bill" ), 20,
+  assert.equal( sommeDesDus( group, "Bill" ), 20,
     "Une seule dépense où j'ai des parts" );
 
-  groupe = [ {
-    parts: [ "Bill", "Billy", "Jim", "Jimmy", "Jimmy" ],
-    prix: 100
+  group = [ {
+    shares: [ "Bill", "Billy", "Jim", "Jimmy", "Jimmy" ],
+    price: 100
   }, {
-    parts: [ "Bill", "Billy", "Tom", "Jimmy", "Jimmy" ],
-    prix: 50
+    shares: [ "Bill", "Billy", "Tom", "Jimmy", "Jimmy" ],
+    price: 50
   } ];
-  assert.equal( sommeDesDus( groupe, "Bill" ), 30,
+  assert.equal( sommeDesDus( group, "Bill" ), 30,
     "Deux dépenses où j'ai des parts" );
-  assert.equal( sommeDesDus( groupe, "Jimmy" ), 60,
+  assert.equal( sommeDesDus( group, "Jimmy" ), 60,
     "Si j'ai deux fois deux parts" );
-  assert.equal( sommeDesDus( groupe, "Tom" ), 10,
+  assert.equal( sommeDesDus( group, "Tom" ), 10,
     "Si certaines dépenses ne me concernent pas" );
 
   assert.end();
 } );
 
-test( "Calcul de la somme des dépenses payées d'une personne sur un groupe", assert => {
-  let groupe;
+test( "Calcul de la somme des dépenses payées d'une personne sur un groupe",
+  assert => {
+    let group;
 
-  groupe = [ {
-    idPayeur: "Bill",
-    prix: 100
-  } ];
-  assert.equal( sommeDesDepenses( groupe, "Bill" ), 100,
-    "Un seul paiement" );
+    group = [ {
+      payerId: "Bill",
+      price: 100
+    } ];
+    assert.equal( sommeDesDepenses( group, "Bill" ), 100,
+      "Un seul paiement" );
+    assert.equal( sommeDesDepenses( group, "John" ), 0,
+      "Un seul paiement qui n'est pas de moi" );
 
-  groupe = [ {
-    idPayeur: "Boule",
-    prix: 100
-  } ];
-  assert.equal( sommeDesDepenses( groupe, "Bill" ), 0,
-    "Un seul paiement qui n'est pas de moi" );
+    group = [ {
+      payerId: "Bill",
+      price: 100
+    }, {
+      payerId: "John",
+      price: 100
+    }, {
+      payerId: "Bill",
+      price: 50
+    } ];
+    assert.equal( sommeDesDepenses( group, "Bill" ), 150,
+      "Plusieurs paiements de moi" );
+    assert.equal( sommeDesDepenses( group, "John" ), 100,
+      "Plusieurs paiements, un seul de moi" );
 
-  groupe = [ {
-    idPayeur: "Bill",
-    prix: 100
-  }, {
-    idPayeur: "Boule",
-    prix: 100
-  }, {
-    idPayeur: "Bill",
-    prix: 50
-  } ];
-  assert.equal( sommeDesDepenses( groupe, "Bill" ), 150,
-    "Plusieurs paiements de moi" );
+    group = [];
+    assert.equal( sommeDesDepenses( group, "Bill" ), 0,
+      "Groupe vide" );
 
-  groupe = [];
-  assert.equal( sommeDesDepenses( groupe, "Bill" ), 0,
-    "Groupe vide" );
-
-  assert.end();
-} );
+    assert.end();
+  } );
